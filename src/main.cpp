ec321f70587a70d9e9a02fa21c7bb456605ae407
NVT2586 <vantinhm82@gmail.com>
2025-07-24 09:24:53 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-//#include <raylib.h>
+#include <raylib.h>
 #include <memory>
 #include <string>
 #include "../include/UI/SoundEffect.h"
@@ -18,67 +18,67 @@
 #include "../include/Item/Star/Star.h"
 #include "../include/Item/One_Up/One_Up.h"
 
-//int main() {
-//    InitWindow(GetScreenWidth(), GetScreenHeight(), "Mario Game Demo");
-//    InitAudioDevice();
-//    SetTargetFPS(60);
-//
-//    GameContext* context = &GameContext::getInstance(); 
-//    MenuState menuState;
-//    GamePlayState gamePlayState;
-//    EditorState editorState;
-//    GameOverState gameOverState;
-//
-//    context->setGameStates(&menuState, &gamePlayState, &editorState, &gameOverState);
-//
-//    while (!WindowShouldClose()) {
-//        float deltaTime = GetFrameTime();
-//        context->handleInput();
-//        context->update(deltaTime);
-//        context->draw();
-//    }
-//
-//    CloseAudioDevice();
-//    CloseWindow();
-//    return 0;
-//}
-
 int main() {
-    InitWindow(600, 400, "Animation");
+    InitWindow(GetScreenWidth(), GetScreenHeight(), "Mario Game Demo");
+    InitAudioDevice();
+    SetTargetFPS(60);
 
-    Texture2D texture = LoadTexture("assets/item/item_spritesheet.png");
+    GameContext* context = &GameContext::getInstance(); 
+    MenuState menuState;
+    GamePlayState gamePlayState;
+    EditorState editorState;
+    GameOverState gameOverState;
 
-	
-    Coin coin({ 300, 100 });
-    Mushroom mushroom({ 200, 100 });
-    Fire_Flower fire_flower({ 100, 100 });
-    Star star({ 400, 100 });
-    One_Up one_up({ 500, 100 });
+    context->setGameStates(&menuState, &gamePlayState, &editorState, &gameOverState);
 
     while (!WindowShouldClose()) {
         float deltaTime = GetFrameTime();
-        coin.update(deltaTime);
-        mushroom.update(deltaTime);
-        fire_flower.update(deltaTime);
-        star.update(deltaTime);
-        one_up.update(deltaTime);
-
-        BeginDrawing();
-        ClearBackground(SKYBLUE);
-
-        coin.draw(texture);
-        mushroom.draw(texture);
-        fire_flower.draw(texture);
-        star.draw(texture);
-        one_up.draw(texture);
-
-        EndDrawing();
+        context->handleInput();
+        context->update(deltaTime);
+        context->draw();
     }
 
-    UnloadTexture(texture);
+    CloseAudioDevice();
     CloseWindow();
-
     return 0;
 }
+
+//int main() {
+//    InitWindow(600, 400, "Item Animation");
+//
+//    Texture2D texture = LoadTexture("assets/item/item_spritesheet.png");
+//
+//	
+//    Coin coin({ 300, 100 });
+//    Mushroom mushroom({ 200, 100 });
+//    Fire_Flower fire_flower({ 100, 100 });
+//    Star star({ 400, 100 });
+//    One_Up one_up({ 500, 100 });
+//
+//    while (!WindowShouldClose()) {
+//        float deltaTime = GetFrameTime();
+//        coin.update(deltaTime);
+//        mushroom.update(deltaTime);
+//        fire_flower.update(deltaTime);
+//        star.update(deltaTime);
+//        one_up.update(deltaTime);
+//
+//        BeginDrawing();
+//        ClearBackground(SKYBLUE);
+//
+//        coin.draw(texture);
+//        mushroom.draw(texture);
+//        fire_flower.draw(texture);
+//        star.draw(texture);
+//        one_up.draw(texture);
+//
+//        EndDrawing();
+//    }
+//
+//    UnloadTexture(texture);
+//    CloseWindow();
+//
+//    return 0;
+//}
 
 
