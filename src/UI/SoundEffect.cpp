e856b94ca68d23eeb2050a81bcadb78d817cb670
Chigiri-Hyoma44 <anhkhoa102006@gmail.com>
2025-08-04 09:03:23 +0700

    change to singleton

--- src/UI/SoundEffect.cpp
+++ src/UI/SoundEffect.cpp
@@ -20,10 +20,12 @@ AudioManager::AudioManager() {
 
     LoadBackgroundMusic("theme1", "./assets/sound/theme1.wav");
 }
+
 AudioManager& AudioManager::getInstance() {
     static AudioManager instance;
     return instance;
 }
+
 AudioManager::~AudioManager() {
     for (auto& sound : sounds) {
         UnloadSound(sound.second);
@@ -48,7 +50,7 @@ void AudioManager::PlaySoundEffect(const std::string& key) {
 }
 
 void AudioManager::SetSoundEffectVolume(float volumePercent) {
-    float volume = volumePercent / 100.0f; // Convert percentage to 0.0f-1.0f range
+    float volume = volumePercent / 100.0f;
     if (volume < 0.0f) volume = 0.0f;
     if (volume > 1.0f) volume = 1.0f;
 
@@ -75,12 +77,10 @@ void AudioManager::PlayBackgroundMusic(const std::string& key) {
             isMusicPlaying[key] = true;
         }
     }
-
     else {
         std::cout << "Music key not found: " << key << "\n";
     }
 }
-
 
 void AudioManager::StopBackgroundMusic(const std::string& key) {
     auto it = backgroundMusics.find(key);
