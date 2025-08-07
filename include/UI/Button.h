882989fbec90a769b3a51c4ec6bd146e52aa3057
Chigiri-Hyoma44 <anhkhoa102006@gmail.com>
2025-08-07 14:42:04 +0700

    Change character selecting effect

--- include/UI/Button.h
+++ include/UI/Button.h
@@ -42,5 +42,20 @@ public:
         Vector2 textSize = MeasureTextEx(UIManager::getInstance().menuFont, c, 50 * scale * 10, 10 * scale * 10);
         DrawTextEx(UIManager::getInstance().menuFont, c, { pos.x - textSize.x / 2.0f, pos.y - textSize.y / 2.0f }, 50 * scale * 10, 10 * scale * 10, WHITE);
     }
+    void draw(Vector2 pos, const char* c, float buttonScale, float textScale) {
+        position.x = pos.x; position.y = pos.y;
+        Texture2D texture = isHovered ? selectedButton : normalButton;
+        float width = texture.width * scale * buttonScale;
+        float height = texture.height * scale * buttonScale;
+        Vector2 drawPos = {
+            position.x - width / 2,
+            position.y - height / 2
+        };
+
+        DrawTextureEx(texture, drawPos, 0, scale * buttonScale, WHITE);
+
+        Vector2 textSize = MeasureTextEx(UIManager::getInstance().menuFont, c, 50 * scale * 10 * textScale, 10 * scale * 10 * textScale);
+        DrawTextEx(UIManager::getInstance().menuFont, c, { pos.x - textSize.x / 2.0f, pos.y - textSize.y / 2.0f }, 50 * scale * 10 * textScale, 10 * scale * 10 * textScale, WHITE);
+    }
 
 };
