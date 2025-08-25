67b93e3214c6bf249af4065d8fc0852433f4f28b
NVT2586 <vantinhm82@gmail.com>
2025-08-25 10:40:05 +0700

    .

--- include/Objects/InteractiveObjects/InteractiveObject.h
+++ include/Objects/InteractiveObjects/InteractiveObject.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "../ObjectFactory.h"
-#include "../System/Interface.h"
+#include "../../System/Interface.h"
 
 class InteractiveObject : public Object, public IUpdatable, public ISavable {
 public:
